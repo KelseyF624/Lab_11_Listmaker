85521f7f427964e3c1e890343c640742c715e314
KelseyF624 <kelseyf.624@gmail.com>
2025-04-01 12:01:14 -0400

    Complete Commit

--- src/ListMaker.java
+++ src/ListMaker.java
@@ -27,38 +27,39 @@ public class ListMaker {
                     break;
 
                 case "Q":
-                    if (SafeInput.getYNConfirm(in, "Are you sure you want to quit?")) {
+                    if (SafeInput.getYNConfirm(in, "Are you sure you want to quit? [Y/N] ")) {
                         done = true;}
                     break; }
 
     }while (!done);
 }
-    private static String displayMenu(Scanner in, ArrayList optionList) {
+private static String displayMenu(Scanner in, ArrayList optionList) {
 
-        if (optionList.isEmpty()) {
-            System.out.println("The list is empty."); }
-        else {
-            System.out.println("The current list is: ");
-            for (int i = 0; i < optionList.size(); i++) {
-                System.out.printf("     %d. %s\n", i + 1,optionList.get(i));}
-        }
+    if (optionList.isEmpty()) {
+        System.out.println("The list is empty."); }
 
-        return SafeInput.getRegExString(in, "Type the letter you wish to select and then press enter.\n" +
-                "A or I: add an item. \n D: Delete an item. \n P: Print the list. \n Q: Quit.", "[AaIiDdPpQq]"); }
-
-    private static void removeItem(Scanner in, ArrayList optionList) {
-
-        int removeItem = SafeInput.getRangedInt(in, "Please type the list number of the item you wish to remove.", 1, optionList.size());
-        optionList.remove(removeItem - 1); }
-
-    private static void addItem(Scanner in, ArrayList optionList){
-
-        String addItem = SafeInput.getNonZeroLenString(in, "Enter the item you want to add: ");
-        optionList.add(addItem); }
-
-    private static void showItems(ArrayList optionList){
-
+    else {
+        System.out.println("The current list is: ");
         for (int i = 0; i < optionList.size(); i++) {
-            System.out.println(optionList.get(i));}
+        System.out.printf("     %d. %s\n", i + 1,optionList.get(i));}
     }
+
+    return SafeInput.getRegExString(in, "Type the letter you wish to select and then press enter.\n" +
+        "A or I: add an item. \n D: Delete an item. \n P: Print the list. \n Q: Quit.", "[AaIiDdPpQq]"); }
+
+private static void removeItem(Scanner in, ArrayList optionList) {
+
+    int removeItem = SafeInput.getRangedInt(in, "Please type the list number of the item you wish to remove.", 1, optionList.size());
+    optionList.remove(removeItem - 1); }
+
+private static void addItem(Scanner in, ArrayList optionList){
+
+    String addItem = SafeInput.getNonZeroLenString(in, "Enter the item you want to add: ");
+    optionList.add(addItem); }
+
+private static void showItems(ArrayList optionList){
+
+    for (int i = 0; i < optionList.size(); i++) {
+    System.out.println(optionList.get(i));}
+}
 }
