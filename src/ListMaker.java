969be604ebcf6e60f3379b0d9a447d012cc28e65
KelseyF624 <kelseyf.624@gmail.com>
2025-04-01 11:36:45 -0400

    partial commit

--- src/ListMaker.java
+++ src/ListMaker.java
@@ -1,8 +1,48 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class ListMaker {
     public static void main(String[] args) {
 
-        
+        ArrayList<String> optionList = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
+        boolean done = false;
+        String items = "";
+
+        do {
+
+            items = displayMenu (in, optionList);
+
+
+
+        }while (!done);
+
 
     }
+    private static String displayMenu(Scanner in, ArrayList optionList) {
+
+
+
+    }
+
+    private static void removeItem(Scanner in, ArrayList optionList) {
+
+
+
+    }
+
+    private static void addItem(Scanner in, ArrayList optionList){
+
+
+
+    }
+
+    private static void showItems(ArrayList optionList){
+
+
+
+    }
+
+
+
 }
